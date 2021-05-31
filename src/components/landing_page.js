1fccf5a7496a1e77cd3fe3c5bf2d7e5ce89ae13e
= <logkashunmughum@gmail.com>
2021-05-31 03:26:02 +0100

    landing page updates

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,13 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Cell, Grid } from "react-mdl";
 
-class LandingPage extends Component{
-    render(){
-        return(
-            <div style ={{width : '100%', margin: 'auto'}}>
-                
+class LandingPage extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          width: "100%",
+          margin: "auto",
+        }}
+      >
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://media.comicbook.com/2020/12/naruto-1249229-1280x0.jpeg"
+              alt="avatar"
+              className="avatar-img"
+            ></img>
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+              <hr />
+              <p>
+                JAVA | SpringBoot | Microservices| React JS| Angular | SQL |
+                MongoDB | AWS
+              </p>
+              <div className="social-links">
+                  <a href="http://www.google.com" rel="noopener noreferrer" target="_blank">
+                  <i class="fa fa-linkedin-in" aria-hidden="true"></i>
+                  </a>
+                  <a href="http://www.google.com" rel="noopener noreferrer" target="_blank">
+                  <i class="fa fa-github-square" aria-hidden="true"></i>
+                  </a>
+               </div>
             </div>
-        )
-    }
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default LandingPage;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class Resume extends Component{
-    render(){
-        return(
-            <div>
-                <h1>
-                    Resume Page
-                </h1>
-            </div>
-        )
-    }
+class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Resume Page </h1>
+      </div>
+    );
+  }
 }
 
 export default Resume;
