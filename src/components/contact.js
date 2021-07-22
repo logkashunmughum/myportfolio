36a76bd19f7d207da6b69c3782de15c2e1df6b34
= <logkashunmughum@gmail.com>
2021-07-22 04:39:14 +0100

    projects page update

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class Contact extends Component{
     render(){
         return(
-            <div>
-                <h1>
-                    Contact Page
-                </h1>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>
+                         <h2> Logka Shunmughum</h2>
+                         <img
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="avatar"
+                            style={{height:250}}    ></img>    
+                            <p style={{width: '75%', margin:'auto', paddingTop:'1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                 when an unknown printer took a galley of type and scramble</p>
+                    
+                    </Cell> 
+                    <Cell col={6}> half page</Cell> 
+
+                </Grid>
+               
             </div>
         )
     }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,15 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Tabs, Tab, Grid, Cell, Card, CardMenu, CardTitle, IconButton, Button, CardText, CardActions} from "react-mdl";
 
-class Projects extends Component{
-    render(){
-        return(
-            <div>
-                <h1>
-                    Projects Page
-                </h1>
-            </div>
-        )
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+   
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+        <Card shadow= {5} style={{minWidth:'450', margin:'auto'}}>
+        <CardTitle style={{color:'#fff', height:'176px', 
+        background: 'url(https://hackernoon.com/hn-images/1*HSisLuifMO6KbLfPOKtLow.jpeg) center / cover'}}>
+        </CardTitle>
+        <CardText>Random Text 1
+        </CardText>
+        <CardActions border>
+            <Button colored> Github</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share">
+            </IconButton>
+        </CardMenu>
+        </Card>
+
+        <Card shadow= {5} style={{minWidth:'450', margin:'auto'}}>
+        <CardTitle style={{color:'#fff', height:'176px', 
+        background: 'url(https://hackernoon.com/hn-images/1*HSisLuifMO6KbLfPOKtLow.jpeg) center / cover'}}>
+        </CardTitle>
+        <CardText>Random Text 2
+        </CardText>
+        <CardActions border>
+            <Button colored> Github</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share">
+            </IconButton>
+        </CardMenu>
+        </Card>
+
+        </div>
+      );
+    } 
+    
+    else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1> This project is done using Java </h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1> This project is done using R </h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1> This project is done using Microservices </h1>
+        </div>
+      );
     }
+  }
+
+  render() {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Java</Tab>
+          <Tab>R</Tab>
+          <Tab>Microservices</Tab>
+        </Tabs>
+
+        <section >
+            <Cell col={12}>
+                <div className='content'>
+                {this.toggleCategories()}
+                </div>
+            </Cell>
+            
+            </section>
+      </div>
+    );
+  }
 }
 
 export default Projects;
